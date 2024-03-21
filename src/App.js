8bde8889ddf0d9b126eddca4753aacf194c2f86b
Jessica Bennett <Jessbennett924@gmail.com>
2024-03-20 21:03:45 -0400

    Update App.js
    Added stage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
+
 import InfiniteScroll from './components/InfiniteScroll';
 
 function App() {
